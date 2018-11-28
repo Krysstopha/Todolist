0331772b9bbe276db5cbbb789e5a957dd0fb0876
Khrysstopha <christopherdedes2906@gmail.com>
2018-11-28 15:23:56 +1100

    Day function and am or pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   // Declare variable for the username
   //var name = prompt("What is your name?"); //---- UN COMMENT
   // Declare variable for the greeting
-  var greeting = "Welcome " /*+ name*/ + ".";
+  var greeting = "Welcome" /*+ name*/ + ".";
   // Inject the greeting to the mainInstruction
-  var mainText = document.getElementById("mainInstruction");
+  var mainText = document.getElementById("mainHeader");
   mainText.textContent = greeting;
   console.log(name + "No Name yet dw"); // Check
   console.log(greeting); // Check
@@ -17,7 +17,7 @@ $(document).ready(function(){
   // Click button to activate function
   $("button").click(function(){
     // Create variable for the div and add class
-    var newItem = "<div class=taskDays contenteditable=true>Hi</div>";
+    var newItem = "<div class=taskDays contenteditable=true>Add a </div>";
     // Add the div to the mainArea
     $("#mainArea").append(newItem);
     // This is a check to make sure it worked
@@ -26,16 +26,30 @@ $(document).ready(function(){
 });
 //-------- End Function
 
-//-------- Day for Task Function (INCOMPLETE)
+//-------- Level of Urgency Function
 $(document).ready(function(){
-  var today = new Date();
-  var day   = today.getDay();
-  var result = document.getElementById("date");
-  result.textContent = day;
-  console.log(today);
-  console.log(day);
+  var urgency
+
 });
 
+//-------- Day for Task Function (INCOMPLETE)
+$(document).ready(function(){
+  var today   = new Date();
+  var weekDay = today.getDay();
+  var day     = today.getDate();
+  var result  = document.getElementById("date");
+
+  // Test which suffix to add to the date
+  if (day == 1 ||  day == 31){
+    result.textContent = day + "st"; // Suffix for first
+  } else if (day == 2 || day == 22){
+    result.textContent = day + "nd"; // Suffix for second
+  } else if (day == 3 || day == 23 ){
+    result.textContent = day + "rd"; // Suffix for third
+  } else {
+    result.textContent = day + "th"; // Suffix for other
+  }
+});
 //-------- End Function
 
 //-------- Check Time Function
@@ -47,19 +61,28 @@ function checkTime(i) {
 
 //-------- Time Function
 function localTime() {
+  // Declaring necessary variables
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
   m = checkTime(m);
   if(h > 12) {
-    h = h-12;
+    h = h-12; // To display time in 12-hour
   }
   else {
-    h = h;
+    h = h;    // To display time in 12-hour
   }
+  // Adding the time to the document
   var result = document.getElementById("time");
   result.textContent = h + ":" + m;
   var t = setTimeout(localTime, 500);
+  // Adding am or pm to the document
+  var h2 = today.getHours(); // 2nd getHours() for a new value
+  var or = document.getElementById("or");
+  if(h2 < 12){
+    or.textContent = "am";
+  } else
+    or.textContent = "pm";
 };
 //-------- End Function
